4d11c2a07555d33f2e87299e0a42e8ce121465cb
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-05 10:32:33 -0300

    hover, click y doble click

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
-	alert("eventos cargados");
-	
+
+
+// MouseOver y MouseOut
+
+let caja = $("#caja");
+/*
+caja.mouseover(function(){
+	$(this).css("background","red");
+});
+
+caja.mouseout(function(){
+	$(this).css("background","green");
+});
+*/
+// Hover
+function cambiaRojo(){
+	$(this).css("background","red");
+}
+function cambiaVerde(){
+	$(this).css("background","green");
+}
+caja.hover(cambiaRojo,cambiaVerde);
+
+
+//Click, Doble click
+
+caja.click(function(){
+	$(this).css("background","blue")
+		   .css("color","white");
+});
+
+caja.dblclick(function(){
+	$(this).css("background","pink")
+		   .css("color","yellow");
+});
+
 });
