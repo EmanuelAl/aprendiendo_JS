f792efa284f52e54498af357606fb2e1514c8f49
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-11 12:27:24 -0300

    uso del sorteable(), en la lista seleccionada

--- aprendiendo-jquery/js/06-interface.js
+++ aprendiendo-jquery/js/06-interface.js
@@ -5,5 +5,28 @@ $(document).ready(function(){
 	$(".elemento").resizable();
 
 	//Seleccionar elementos de una lista
-	$(".lista-seleccionable").selectable();
+	//$(".lista-seleccionable").selectable();
+
+
+
+	//ordenar elementos de una lista
+	/*para ordenar hay que "comentar" la linea
+	de codigo del selectable, o no se aplica
+	los cambios de sortable*/
+// 	$(".lista-seleccionable").sortable();
+// });
+	/*sorteable() tiene un propiedad update
+	que se le puede pasar un callback
+	para capturar cuando el evento se aplico
+	por ejemplo.
+*/
+	$(".lista-seleccionable").sortable({
+		update: function(event,ui){
+			console.log("ha cambiado la lista");
+		}
+	});
 });
+	
+
+
+	
