772adb1865ff74e5e05a0422fa63cc406902468f
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-16 08:49:25 -0300

    cambiador de temas

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -57,5 +57,18 @@ let posts = [
 		$("#posts").append(post);
 	});
 	
+	let theme = $("#theme");
+	$("#to-green").click(function(){
+		theme.attr("href","css/green.css");
+	});
+	$("#to-red").click(function(){
+		theme.attr("href","css/red.css");
+	});
+	$("#to-blue").click(function(){
+		theme.attr("href","css/blue.css");
+	});
+
+
+
 
 });
