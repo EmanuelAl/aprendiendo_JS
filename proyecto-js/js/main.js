d76bc15722e066dd123dca72490eeed926012c09
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-15 23:50:54 -0300

    listado dinamico de articulos / posts , cargando un json

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 	
-
+//slider
 //Inicializo el bxslider
 	 $('.slider').bxSlider({
     mode: 'fade',
@@ -9,6 +9,53 @@ $(document).ready(function(){
     slideWidth: 1200,
   });
 
+// Posts
+let posts = [
+	{
+		title: "prueba de titulo 1",
+		date: 'Publicado el dia ' + moment().date()+ ' de '+moment().format("MMMM")+ ' del '+moment().format("YYYY"),
+		content: "Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Sint dolorem illo aliquid. Id, ipsa, nostrum! Recusandae enim voluptas pariatur earum deserunt autem quisquam maxime voluptatibus odit, culpa corporis est eaque! Est earum modi consectetur reprehenderit, fugiat maiores obcaecati dignissimos nulla suscipit voluptates, ducimus deleniti mollitia veniam corrupti enim accusamus corporis magnam impedit id saepe totam incidunt temporibus aspernatur? Tempore, impedit."
+	},
+	{
+		title: "prueba de titulo 2",
+		date: 'Publicado el dia ' + moment().date()+ ' de '+moment().format("MMMM")+ ' del '+moment().format("YYYY"),
+		content: "Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Sint dolorem illo aliquid. Id, ipsa, nostrum! Recusandae enim voluptas pariatur earum deserunt autem quisquam maxime voluptatibus odit, culpa corporis est eaque! Est earum modi consectetur reprehenderit, fugiat maiores obcaecati dignissimos nulla suscipit voluptates, ducimus deleniti mollitia veniam corrupti enim accusamus corporis magnam impedit id saepe totam incidunt temporibus aspernatur? Tempore, impedit."
+	},
+	{
+		title: "prueba de titulo 3",
+		date: 'Publicado el dia ' + moment().date()+ ' de '+moment().format("MMMM")+ ' del '+moment().format("YYYY"),
+		content: "Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Sint dolorem illo aliquid. Id, ipsa, nostrum! Recusandae enim voluptas pariatur earum deserunt autem quisquam maxime voluptatibus odit, culpa corporis est eaque! Est earum modi consectetur reprehenderit, fugiat maiores obcaecati dignissimos nulla suscipit voluptates, ducimus deleniti mollitia veniam corrupti enim accusamus corporis magnam impedit id saepe totam incidunt temporibus aspernatur? Tempore, impedit."
+	},
+	{
+		title: "prueba de titulo 4",
+		date: 'Publicado el dia ' + moment().date()+ ' de '+moment().format("MMMM")+ ' del '+moment().format("YYYY"),
+		content: "Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Sint dolorem illo aliquid. Id, ipsa, nostrum! Recusandae enim voluptas pariatur earum deserunt autem quisquam maxime voluptatibus odit, culpa corporis est eaque! Est earum modi consectetur reprehenderit, fugiat maiores obcaecati dignissimos nulla suscipit voluptates, ducimus deleniti mollitia veniam corrupti enim accusamus corporis magnam impedit id saepe totam incidunt temporibus aspernatur? Tempore, impedit."
+	},
+	{
+		title: "prueba de titulo 5",
+		date: 'Publicado el dia ' + moment().date()+ ' de '+moment().format("MMMM")+ ' del '+moment().format("YYYY"),
+		content: "Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Sint dolorem illo aliquid. Id, ipsa, nostrum! Recusandae enim voluptas pariatur earum deserunt autem quisquam maxime voluptatibus odit, culpa corporis est eaque! Est earum modi consectetur reprehenderit, fugiat maiores obcaecati dignissimos nulla suscipit voluptates, ducimus deleniti mollitia veniam corrupti enim accusamus corporis magnam impedit id saepe totam incidunt temporibus aspernatur? Tempore, impedit."
+	},
+	{
+		title: "prueba de titulo 6",
+		date: 'Publicado el dia ' + moment().date()+ ' de '+moment().format("MMMM")+ ' del '+moment().format("YYYY"),
+		content: "Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Sint dolorem illo aliquid. Id, ipsa, nostrum! Recusandae enim voluptas pariatur earum deserunt autem quisquam maxime voluptatibus odit, culpa corporis est eaque! Est earum modi consectetur reprehenderit, fugiat maiores obcaecati dignissimos nulla suscipit voluptates, ducimus deleniti mollitia veniam corrupti enim accusamus corporis magnam impedit id saepe totam incidunt temporibus aspernatur? Tempore, impedit."
+	}
+];
+	
+	posts.forEach((element, index ) => {
+		let post = `
+		<article class="post">
+		    <h2>${element.title}</h2>
+			<span class="date">${element.date}</span>
+			<p>${element.content}	
+			</p>
+			<a href="#" class="button-more">Leer más</a>
+		</article>
+		`;
 
+		$("#posts").append(post);
+	});
+	
 
 });
