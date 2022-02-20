b0955247f22c76e550aecb75f85c2fc86e1a76a1
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-20 18:12:05 -0300

    let vs var

--- poo-y-typescript/js/03-tipos-de-datos.js
+++ poo-y-typescript/js/03-tipos-de-datos.js
@@ -18,4 +18,17 @@ var lenguajes = ['PHP', 'JS', 'css', 12];
 //otra forma de definir un array
 var years = [12, 13, 14];
 // let years: any[] = [12, 13, 14,'hola']; otro ejemplo
-console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
+/************************/
+//explicacion de usar let vs var 
+//let funciona a nivel de bloque
+//var funciona a nivel global
+var numero1 = 10;
+var numero2 = 12;
+if (numero1 == 10) {
+    var numero1_1 = 44; //existe dentro del if 
+    var numero2 = 55; //global modifica la variable por fuera del if
+    console.log(numero1_1, numero2); //imprimira 44 y 55
+}
+console.log(numero1, numero2); //imprimira 10 y 55
+//********************* */
+console.log(cadena, cadena2, numero, verdadero_falso, cualquiera, lenguajes, years);
--- poo-y-typescript/js/03-tipos-de-datos.ts
+++ poo-y-typescript/js/03-tipos-de-datos.ts
@@ -24,7 +24,21 @@ var lenguajes: Array<any> = ['PHP','JS','css',12];
 //otra forma de definir un array
 let years: number[] = [12, 13, 14];
 // let years: any[] = [12, 13, 14,'hola']; otro ejemplo
+/************************/
+//explicacion de usar let vs var 
+//let funciona a nivel de bloque
+//var funciona a nivel global
 
+var numero1 = 10;
+var numero2 = 12;
 
-console.log(cadena,numero,verdadero_falso,cualquiera,lenguajes, years);
+if(numero1==10){
+    let numero1 = 44;//existe dentro del if 
+    var numero2 = 55;//global modifica la variable por fuera del if
+    console.log(numero1, numero2);//imprimira 44 y 55
+}
+console.log(numero1, numero2);//imprimira 10 y 55
+
+//********************* */
+console.log(cadena,cadena2,numero,verdadero_falso,cualquiera,lenguajes, years);
 
