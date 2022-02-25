1afe1dffa29f702d7af19599424b69b0bb81d09d
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-25 10:51:45 -0300

    models + import y export

--- aprendiendo-angular/src/app/app.component.ts
+++ aprendiendo-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Configuracion } from './models/configuracion';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Welcome to';
+  public descripcion: string;
   public mostrar_videojuegos: boolean = true;
+  public config: any;
+
+  constructor(){
+    this.title = Configuracion.titulo;
+    this.descripcion = Configuracion.descripcion;
+    this.config = Configuracion;
+  }
+
 
   ocultarVideojuego(value: any){
     this.mostrar_videojuegos = value;
---
+++ aprendiendo-angular/src/app/models/configuracion.ts
@@ -0,0 +1,14 @@
+/* los modelos de datos son simplemente
+unas clases con unas propiedades
+que representan una entidad
+como la entidad 'zapatilla'
+la entidad 'videojuego', la entidad 'curso',
+lo que sea, y a partir de esa
+clase poder crear luego diferentes
+objetos */
+export var Configuracion = {
+    color: "red",
+    fondo: "#22aa80",
+    titulo: "Aplicacion con Angular en",
+    descripcion: "Aprendiendo Angular con Victor Robles"
+};
