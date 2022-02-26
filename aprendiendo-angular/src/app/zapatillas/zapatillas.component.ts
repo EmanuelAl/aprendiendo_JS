3be8d6807ba9e6e7f238c307aa2b249bb8cfecae
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-26 17:38:33 -0300

    directivas : uso del ngFor ngIf y directivas de atributo

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -8,18 +8,34 @@ import { Zapatilla } from "../models/zapatilla";
 export class ZapatillasComponent implements OnInit {
     public titulo: string = "Componente de zapatillas";
     public zapatillas: Array<Zapatilla>;
+    public marcas: String[];
 
     constructor(){
+        this.marcas= new Array();
+
         this.zapatillas = [
             new Zapatilla('Nike Airmax','Nike','Rojas', 40, true),
             new Zapatilla('Reebook Classic','Reebook','Blanco', 80, true),
+            new Zapatilla('Reebook Spartan','Reebook','Negra', 180, true),
             new Zapatilla('Nike Runner MD','Nike','Negras', 60, true),
             new Zapatilla('Adidas Yezzy','Adidas','Gris', 180, false)
         ];
-
     }
 
     ngOnInit(): void {
         console.log(this.zapatillas);
+
+        this.getMarcas();
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla,index)=>{
+
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+            }
+            
+        });
+        console.log(this.marcas);
     }
 }
