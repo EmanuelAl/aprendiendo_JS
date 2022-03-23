9da4a87e2944c719175d324a220c95b29d9c1897
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-03-22 22:49:41 -0300

    explicacion y uso del decorador viewchild para hacer selecciones en el DOM

--- proyecto-angular/src/app/components/contact/contact.component.ts
+++ proyecto-angular/src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit , ViewChild} from '@angular/core';
     
 @Component({
   selector: 'app-contact',
@@ -10,14 +10,27 @@ export class ContactComponent implements OnInit {
   public anchuraToSlider: any;
   public captions: boolean;
   public autor: any;
+  /*                   {static: true/false} , es true si la vista(html) no depende de ningun condicional
+                                              es false si depende la vista(html) de alguna condicional*/
+  @ViewChild('textos',{static: true}) textos: ElementRef | any//para utilizar la seleccion de la vista
 
   constructor() { 
     // this.widthSlider = 0;
     this.captions = true;
+
   }
 
   ngOnInit(): void {
- 
+    let opcion_clasica = document.querySelector('#texto')?.innerHTML;//manera clasica de seleccionar un elemento del DOM con javascript 
+    //en angular para seleccionar algun elemento del DOM podemos usar los viewchilds:
+    /*
+    -- en la vista creamos la variable a seleccionar con #nombrevariable
+    -- en el componente se importa la clase ViewChild
+    -- se crea en el componente una propiedad nueva con el decorador @ViewChild('nombrevariable')
+     */
+    console.log(this.textos);
+    alert(this.textos.nativeElement.textContent);
+
   }
 
   cargarSlider(){
